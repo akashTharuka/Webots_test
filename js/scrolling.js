ad423eac43523289c8465d324be053bd0ee475b1
tharukaeaa <akash.19@cse.mrt.ac.lk>
2022-01-26 20:06:52 +0530

    asd

--- js/scrolling.js
+++ js/scrolling.js
@@ -11,7 +11,8 @@ window.sr = ScrollReveal(config);
 const revealLeft = {
     origin   : 'right',
     delay    : 200,
-    distance : '120px',
+    duration: 500,
+    distance : '60px',
     easing   : 'ease-in-out',
     viewFactor: 0.1
 };
@@ -19,7 +20,8 @@ const revealLeft = {
 const revealRight = {
     origin   : 'left',
     delay    : 200,
-    distance : '120px',
+    duration: 500,
+    distance : '60px',
     easing   : 'ease-in-out',
     viewFactor: 0.1
 }
@@ -38,6 +40,24 @@ const revealRotate = {
     easing: 'ease-in-out'
 };
 
+const revealRotate1 = {
+    rotate: { x: 0, y: 180, z: 0 },
+    delay: 400,
+    easing: 'ease-in-out'
+};
+
+const revealRotate2 = {
+    rotate: { x: 0, y: 180, z: 0 },
+    delay: 600,
+    easing: 'ease-in-out'
+};
+
+const revealRotate3 = {
+    rotate: { x: 0, y: 180, z: 0 },
+    delay: 800,
+    easing: 'ease-in-out'
+};
+
 const revealFlip = {
     rotate: { x: 180, y: 0, z: 0 },
     delay: 200,
@@ -48,6 +68,9 @@ sr.reveal('.reveal-left', revealLeft);
 sr.reveal('.reveal-right', revealRight);
 sr.reveal('.reveal-bottom', revealBottom);
 sr.reveal('.reveal-rotate', revealRotate);
+sr.reveal('.reveal-rotate-400', revealRotate1);
+sr.reveal('.reveal-rotate-600', revealRotate2);
+sr.reveal('.reveal-rotate-800', revealRotate3);
 sr.reveal('.reveal-flip', revealFlip);
 
 // set the navbar background after scrolling
