3ee8802e927e2ad583a9fd94c53edc5e4900e359
tharukaeaa <akash.19@cse.mrt.ac.lk>
2022-03-04 00:45:25 +0530

    secured passwords

--- server/app.js
+++ server/app.js
@@ -4,15 +4,22 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const leaderboardRoutes = require('./routes/leaderboardRoutes');
 
+// environmental variables
+require('dotenv').config();
+
 // express app
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// mongodb username - Kalana99, password - Kalana99
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://Kalana99:Kalana99@cluster0.lrpxz.mongodb.net/leaderBoards?retryWrites=true&w=majority";
+
+const dbPswAkash = process.env.DB_PSW_AKASH;
+const dbPswKalana = process.env.DB_PSW_KALANA;
+
+// const dbURI = `mongodb+srv://Kalana99:${dbPswKalana}@cluster0.lrpxz.mongodb.net/leaderBoards?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://admin:${dbPswAkash}@cluster0.lndvt.mongodb.net/leadersboard?retryWrites=true&w=majority`;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(5000, () => {
