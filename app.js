618d805df67b7bccedfbf432f0cf48858997a089
tharukaeaa <akash.19@cse.mrt.ac.lk>
2022-03-05 00:51:02 +0530

    asd

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ const dbPswAkash = process.env.DB_PSW_AKASH;
 const dbPswKalana = process.env.DB_PSW_KALANA;
 
 // const dbURI = process.env.DB_URL_2;
-const dbURI = process.env.DB_URL_1;
+const dbURI = `mongodb+srv://admin:${process.env.DB_PSW}@cluster0.lndvt.mongodb.net/leaderboard?retryWrites=true&w=majority`;
+
+// middleware & static files
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'));
@@ -31,12 +34,10 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 	}))
 	.catch(err => console.log(err));
 
-// register view engine
-app.set('view engine', 'ejs');
 
-// middleware & static files
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+
+// app.use(morgan('dev'));
+
 app.use((req, res, next) => {
 	res.locals.path = req.path;
 	next();
@@ -44,8 +45,3 @@ app.use((req, res, next) => {
 
 
 app.use('/leaderboards', leaderboardRoutes);
-
-// 404 page
-// app.use((req, res) => {
-//   res.status(404).render('404', { title: '404' });
-// });
