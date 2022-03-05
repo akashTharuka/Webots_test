3a33dcc27f6cedba7ec5e03d3d4385647d01902a
tharukaeaa <akash.19@cse.mrt.ac.lk>
2022-03-05 10:28:03 +0530

    setup heroku config vars

--- app.js
+++ app.js
@@ -13,13 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// connect to mongodb & listen for requests
 
-const dbPswAkash = process.env.DB_PSW_AKASH;
-// const dbPswKalana = process.env.DB_PSW_KALANA;
-
-// const dbURI = process.env.DB_URL_2;
-const dbURI = `mongodb+srv://admin:admin1234@cluster0.lndvt.mongodb.net/leaderboard?retryWrites=true&w=majority`;
+const dbURI = process.env.DB_URI;
 
 // middleware & static files
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +23,7 @@ if (process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'));
 }
 
+// connect to mongodb and listen
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(result => app.listen(process.env.PORT || 5000, () => {
 		console.log('listening to port 5000');
