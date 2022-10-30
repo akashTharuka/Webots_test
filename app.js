1c35c2870cdb96b593159f8f911e9e0d497bd46f
Akash <akash_tharuka@yahoo.com>
2022-10-30 20:28:51 +0530

    cors error

--- app.js
+++ app.js
@@ -29,17 +29,22 @@ app.listen(process.env.PORT || 5000, () => {
 })
 
 // connect to mongodb and listen
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 	.then(result => app.listen(process.env.PORT || 5000, () => {
-// 		console.log('listening to port 5000');
-// 	}))
-// 	.catch(err => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(result => app.listen(process.env.PORT || 5000, () => {
+		console.log('listening to port 5000');
+	}))
+	.catch(err => console.log(err));
 
 app.use((req, res, next) => {
 	res.locals.path = req.path;
 	next();
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 app.use('/leaderboards', leaderboardRoutes);
 app.use('/dashboard', dashboardRoutes);
