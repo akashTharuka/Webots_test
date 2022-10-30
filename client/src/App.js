feca56b617071500a2f310a44a2ef56c3aec568e
Akash <akash_tharuka@yahoo.com>
2022-10-30 21:17:20 +0530

    routing refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Navbar, Homepage, LiveStream } from './components';
 import { Login, Dashboard } from './dashboard_components';
