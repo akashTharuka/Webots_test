b5655ccb284ef21d2ca38bf866fe293b12e54ad0
Akash <akash_tharuka@yahoo.com>
2022-11-09 10:26:45 +0530

    register time changed

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const Register = () => {
 
     const calculateTimeLeft = () => {
-        let countDownDate = new Date("Nov 11, 2022 00:00:00").getTime();
+        let countDownDate = new Date("Nov 11, 2022 10:30:00").getTime();
 
         let now = new Date().getTime();
 
