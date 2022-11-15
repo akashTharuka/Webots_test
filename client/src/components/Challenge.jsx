89cdc0522d36fddb63f1064fd9bd9dcef3a91353
Akash <akash_tharuka@yahoo.com>
2022-11-15 06:56:20 +0530

    task documents updated

--- client/src/components/Challenge.jsx
+++ client/src/components/Challenge.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import challenge_pdf from '../resources/IESL Robogames - Challenge 1.pdf';
+import challenge_zip from '../resources/Robogames_Challenge_1.zip';
 
 const Challenge = () => {
     return(
@@ -14,7 +16,7 @@ const Challenge = () => {
                     </div>
                 </div>
 
-                {/* <div className="row justify-content-between">
+                <div className="row justify-content-between">
 
                     <div className="row mt-5 reveal-flip">
                         <div className="col-lg-8 mx-auto text-center text-muted reveal-bottom">
@@ -26,23 +28,23 @@ const Challenge = () => {
                     </div>
 
                     <div className="col-lg-6 mx-auto my-3 text-center">
-                        <Link className="neon-button-three btn p-3 px-5 mt-3 mx-4 btn-lg reveal-right" to="resources/RoboGames-University-Category.pdf" target="_blank">
+                        <a className="neon-button-three btn p-3 px-5 mt-3 mx-4 btn-lg reveal-right" href={challenge_pdf} target="_blank" rel='noreferrer' download>
                             <span></span>
                             <span></span>
                             <span></span>
                             <span></span>
-                            Univercity Category!</Link>
-                        <Link className="neon-button-three btn p-3 px-5 mt-3 mx-4 btn-lg reveal-left" to="resources/RoboGames-School-Category.pdf" target="_blank">
+                            Challenge Description</a>
+                        <a className="neon-button-three btn p-3 px-5 mt-3 mx-4 btn-lg reveal-left" href={challenge_zip}  target="_blank" rel='noreferrer' download>
                             <span></span>
                             <span></span>
                             <span></span>
                             <span></span>
-                            School Category!</Link>
+                            Challenge Files</a>
                     </div>
 
                 </div>
 
-                <div className="row justify-content-between">
+                {/* <div className="row justify-content-between">
 
                     <div className="row mt-5 reveal-flip">
                         <div className="col-lg-8 mx-auto text-center text-muted reveal-bottom">
@@ -63,7 +65,7 @@ const Challenge = () => {
                     </div>
                     
                 </div> */}
-				<div className="lead topic text-uppercase text-warning text-center my-5">Will be available soon!!!</div>
+				{/* <div className="lead topic text-uppercase text-warning text-center my-5">Will be available soon!!!</div> */}
             </div>
         </section>
     );
