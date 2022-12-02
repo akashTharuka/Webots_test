9fb3e81130b86bee3cc53cbdbb0bdc9b93d0a373
Kalana99 <kalanarajika99@gmail.com>
2022-12-02 16:28:11 +0530

    final challenge

--- client/src/components/Challenge.jsx
+++ client/src/components/Challenge.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import challenge_pdf from '../resources/IESL Robogames - Challenge 1_version_3.pdf';
-import challenge_zip from '../resources/Robogames_Challenge_1_version_3.zip';
+import challenge_pdf from '../resources/IESL Robogames - Final Challenge.pdf';
+import challenge_zip from '../resources/Robogames - Final Challenge.rar';
 
 const Challenge = () => {
     return(
